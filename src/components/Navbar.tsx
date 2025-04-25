edf922dbeb03c32908728dc15518057d101bdc20
Rohitparakh <rohitparakh4@gmail.com>
2025-04-25 16:33:36 +0530

    Added about in navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     { name: 'Home', path: '/' },
     { name: 'Services', path: '/services' },
     { name: 'Portfolio', path: '/portfolio' },
-    // { name: 'About', path: '/about' },
+    { name: 'About', path: '/about' },
     { name: 'Contact', path: '/contact' },
   ];
 
